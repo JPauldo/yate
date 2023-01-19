cfc6e2372d8700123a4120f05db9bede0a4190f8
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-19 18:33:07 -0500

    Added logic to a method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,19 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  // console.error('putDb not implemented');
+  console.log('PUT to the database');
+  
+  const jateDB = await jateDB('jateDB', 1);
+  const tx = jateDB.transaction('content', 'readonly');
+  const request = tx.store.put({ id: 1, value: content });
+  const result = await request;
+
+  console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   // console.error('getDb not implemented');
-  console.log('Start of GET');
+  console.log('GET from the database');
   
   const jateDB = await jateDB('jateDB', 1);
   const tx = jateDB.transaction('content', 'readonly');
   const request = tx.store.get(1);
   const result = await request;
   
-  console.log('End of GET');
+  console.log('result.value', result);
   return result.value;
 };
 
