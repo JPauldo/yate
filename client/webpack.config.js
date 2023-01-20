c876180e76a7141da6c657efe6191dd4ee39e520
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-20 09:56:11 -0500

    Added and configured workbox plugins for a service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Y.A.T.E.'
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: './service-worker.js'
+      }),
+      new MiniCssExtractPlugin(),
+      new WebpackPwaManifest({
+        name: 'Yet Another Text Another',
+        short_name: 'Y.A.T.E.',
+        description: 'Similar to YAML but with a text editor!',
+        background_color: '#F0F8FF',
+        theme_color: '#F0F8FF',
+        start_url: './',
+        public: './',
+        fingerprints: false,
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+          }
+        ]
+      })
     ],
 
     module: {
